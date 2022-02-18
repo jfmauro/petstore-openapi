76c8c4dd744c4cd8f21f992f8564443f7aebc342
MAURO JEAN-FRANCOIS <jeanfrancois.mauro@minfin.fed.be>
2022-02-18 09:36:06 +0100

    added angular generation

--- src/main/java/be/petstoreapifirst/pet/api/PetExceptionHandler.java
+++ src/main/java/be/petstoreapifirst/pet/api/PetExceptionHandler.java
@@ -1,6 +1,7 @@
-package be.petstoreapifirst.pet.service;
+package be.petstoreapifirst.pet.api;
 
 import be.petstoreapifirst.pet.exception.CategoryNotFoundException;
+import be.petstoreapifirst.pet.exception.PetNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,13 +10,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletResponse;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class PetExceptionHandler {
 
     //makes Spring return the same HTTP status that you received
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CategoryNotFoundException.class)
-    public String handleFeignStatusException(CategoryNotFoundException e, HttpServletResponse response) {
+    public String handleCategoryNotFound(CategoryNotFoundException e, HttpServletResponse response) {
         //response.setStatus(e.status());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(PetNotFoundException.class)
+    public String handlePetNotFound(PetNotFoundException e, HttpServletResponse response) {
         return e.getMessage();
     }
 
